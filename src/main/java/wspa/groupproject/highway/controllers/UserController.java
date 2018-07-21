c4540cdf176e8bb3b3245c3f912720bb119f026e
Sylwester Wróblewski <sylwek.wroblewski@gmail.com>
2018-07-21 21:24:54 +0200

    Users refactor

--- src/main/java/wspa/groupproject/highway/controllers/UserController.java
+++ src/main/java/wspa/groupproject/highway/controllers/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import wspa.groupproject.highway.model.Role;
 import wspa.groupproject.highway.model.RoleName;
 import wspa.groupproject.highway.model.User;
 import wspa.groupproject.highway.repository.RoleRepository;
@@ -13,6 +15,7 @@ import wspa.groupproject.highway.repository.UserRepository;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController()
 public class UserController {
@@ -25,10 +28,26 @@ public class UserController {
     private RoleRepository roleRepository;
 
     @GetMapping(PATH)
-    @Secured("ROLE_ADMIN")
+    @Secured({"ROLE_ADMIN", "ROLE_INSTRUCTOR", "ROLE_USER"})
     @ResponseStatus(value = HttpStatus.ACCEPTED)
     public List<User> usersGET() {
-        return userRepository.findAll();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<String> roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        List<User> users = userRepository.findAll();
+        if (roles.contains("ROLE_ADMIN")) {
+            return users;
+        } else if (roles.contains("ROLE_USER")) {
+            return users.stream()
+                    .filter(this::isInstructor)
+                    .collect(Collectors.toList());
+        } else if (roles.contains("ROLE_INSTRUCTOR")) {
+            return users.stream()
+                    .filter(this::isUser)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 
     @PostMapping(PATH)
@@ -61,5 +80,17 @@ public class UserController {
         updated.setPassword(user.getPassword());
         updated.setEmail(user.getEmail());
         userRepository.updateUser(updated);
+    }
+
+    private boolean isInstructor(User user) {
+        return user.getRoles().stream()
+                .map(Role::getName)
+                .anyMatch(RoleName.ROLE_INSTRUCTOR::equals);
+    }
+
+    private boolean isUser(User user) {
+        return user.getRoles().stream()
+                .map(Role::getName)
+                .anyMatch(RoleName.ROLE_INSTRUCTOR::equals);
     }
 }
